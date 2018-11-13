ee446bc79bd553ef8ad5e436e35979c8cb553c92
Tomas Doherty <tomasdoh1@gmail.com>
2018-11-13 19:47:32 +0000

    Hit point feature test passing

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,8 +1,10 @@
 
 feature 'hit points' do
-  scenario "view each player's hit points" do
-    visit '/play'
-    click_on 'Player 2 hit points'
-    expect(page).to have_content 'Player 2 hit points: 25'
+  scenario "view Player 2's hit points" do
+    visit '/'
+    fill_in :player1, with: 'Luca'
+    fill_in :player2, with: 'Tomas'
+    click_button 'Submit'
+    expect(page).to have_content 'Tomas: 25HP'
     end
   end
