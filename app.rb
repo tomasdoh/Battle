5dd15f3cf25b396b92ad9cfb3f24b9751f15580f
Tomas Doherty <tomasdoh1@gmail.com>
2018-11-15 16:31:18 +0000

    refactoring controller

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require 'sinatra/base'
 require 'shotgun'
-require_relative 'lib/player'
+require './lib/player'
+require './lib/game'
 
 class Battle < Sinatra::Base
   enable :sessions
@@ -10,21 +11,20 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    $player1 = Player.new(params[:player1])
-    $player2 = Player.new(params[:player2])
+    player1 = Player.new(params[:player1])
+    player2 = Player.new(params[:player2])
+    $game = Game.new(player1, player2)
     redirect '/play'
   end
 
   get '/play' do
-    @player1 = $player1
-    @player2 = $player2
+    @game = $game
     erb(:play)
   end
 
   get '/attack' do
-    @player1 = $player1
-    @player2 = $player2
-    Game.new.attack(@player2)
+    @game = $game
+    @game.attack(@game.player2)
     erb(:attack)
   end
 
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,14 +1,26 @@
 require 'game'
 
 describe Game do
-  subject(:game) { described_class.new }
-  let(:tomas) { double :player }
-  let(:luca) { double :player }
+  subject(:game) { described_class.new(player1, player2) }
+  let(:player1) { double :player }
+  let(:player2) { double :player }
+
+  describe '#player_1' do
+    it 'retrieves the first player' do
+      expect(game.player1).to eq player1
+    end
+  end
+
+  describe '#player_2' do
+    it 'retrieves the second player' do
+      expect(game.player2).to eq player2
+    end
+  end
 
   describe '#attack' do
     it 'damages the player' do
-      expect(tomas).to receive(:receive_damage)
-      game.attack(tomas)
+      expect(player1).to receive(:receive_damage)
+      game.attack(player1)
     end
   end
 end
